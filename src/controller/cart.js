6ff968a86032e08759ea4a3a1e34591b99e31f28
Reskyppo <reskybeen@gmail.com>
2020-12-02 23:11:21 +0700

    fix bug find not func

--- src/controller/cart.js
+++ src/controller/cart.js
@@ -2,15 +2,13 @@ const Cart = require("../models/cart");
 
 exports.addItemToCart = (req, res) => {
   Cart.find({ user: req.user._id }).exec((err, cart) => {
+    console.log(req.body.cartItems.product);
     if (err) return res.status(400).json({ message: err });
-    if (cart) {
-      // update quantity when cart exist
-
+    if (cart.length > 0) {
+      // update quantoty when cart exist
       const product = req.body.cartItems.product;
-      const item = cart.cartItems.find((c) => {
-        c.product === product;
-      });
-
+      const item = cart[0].cartItems.find((c) => c.product == product);
+      console.log(item);
       if (item) {
         Cart.findOneAndUpdate(
           { user: req.user._id, "cartItems.product": product },
@@ -44,10 +42,11 @@ exports.addItemToCart = (req, res) => {
         user: req.user._id,
         cartItems: [req.body.cartItems],
       });
-
-      cart.save((err, cart) => {
-        if (err) return res.status(400).json({ message: err });
-        if (cart) return res.status(200).json({ message: cart });
+      cart.save((error, cart) => {
+        if (error) return res.status(400).json({ error });
+        if (cart) {
+          return res.status(201).json({ data: cart });
+        }
       });
     }
   });
